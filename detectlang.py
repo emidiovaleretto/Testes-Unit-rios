07d07812af532a2073f2ea4d86d226fc0d55622c
Emidio Valereto <53981181+EmidioDeveloper@users.noreply.github.com>
2020-06-01 13:19:11 +0100

    Update detectlang.py

--- detectlang.py
+++ detectlang.py
@@ -1,27 +1,27 @@
 from langdetect import detect
 
+
 class LangDetect:
 
     def __init__(self, text):
         self.text = text
 
     def predict(self):
-
         """
         Função verifica se o parâmetro texto é do tipo String.
 
-            >>> lang_detect = LangDetect('“yeah, I’m young, rich, beautiful, and famous. People think rich people are happy.“')
-            >>> lang_detect.predict();
-            'en'
+        >>> lang_detect = LangDetect('“I’m young, rich, beautiful, and famous. People think rich people are happy.“')
+        >>> lang_detect.predict()
+        'en'
 
-            >>> lang_detect = LangDetect('La navicella è entrata in orbita e viaggia verso la Stazione spaziale internazionale (Iss).');
-            >>> lang_detect.predict();
-            'it'
+        >>> lang_detect = LangDetect('La navicella è entrata in orbita e viaggia verso la Stazione spaziale internazionale')
+        >>> lang_detect.predict()
+        'it'
 
-            >>> lang_detect = LangDetect('Dois ou um');
-            >>> lang_detect.predict();
-            'pt'
-            
+        >>> lang_detect = LangDetect('Dois ou um')
+        >>> lang_detect.predict()
+        'pt'
+
         """
 
         assert isinstance(self.text, str), 'O valor digitado não pode ser de outro tipo, exceto String.'
@@ -30,4 +30,5 @@ class LangDetect:
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod(verbose=True)
